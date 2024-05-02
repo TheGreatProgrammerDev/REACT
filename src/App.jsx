44ae1149ee41bf2dc9d3de6bc4fc29097e3b7e1e
TheGreatProgrammerDev <168670849+TheGreatProgrammerDev@users.noreply.github.com>
2024-05-02 04:05:57 +0000

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+// import Title from "./Title";
+// import Title from "/.Title.jsx"
+// import ProductTab from "./ProductTab.jsx"
+import Product from "./Product";
+
+function App () {
+    return (
+        <>
+        <Product />
+        <Product />
+        <Product />
+        </>
+    )
+}
+
+export default App;
---
+++ src/Title.jsx
@@ -0,0 +1,10 @@
+function Title () {
+    return (
+        <div>
+            <h1>i am the title</h1>
+            <h3>i am the best</h3>
+        </div>
+    )
+}
+
+export default Title;
